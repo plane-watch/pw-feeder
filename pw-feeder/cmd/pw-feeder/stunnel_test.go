59e1d45a9f671509e66f9c410e8bdccf67d40a7e
Mike Nye <mike.nye@gmail.com>
2023-05-28 19:41:05 +0800

    add test for stunnelConnect

--- pw-feeder/cmd/pw-feeder/stunnel_test.go
+++ pw-feeder/cmd/pw-feeder/stunnel_test.go
@@ -4,17 +4,40 @@ import (
 	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"math/big"
 	"net"
+	"sync"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
-// TODO: finish writing tests for this module
-func prepCertsForTesting(t *testing.T) error {
+func connHandler(t *testing.T, conn net.Conn) {
+	// handles incoming connections
+
+	var (
+		sendRecvBufferSize = 256 * 1024 // 256kB
+	)
+
+	defer conn.Close()
+
+	buf := make([]byte, sendRecvBufferSize)
+	for {
+
+		// read data
+		_, err := conn.Read(buf)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func prepCertsForTesting(t *testing.T) (certPEM, certPrivKeyPEM, caPEM *bytes.Buffer, err error) {
 
 	// prep CA cert for testing
 	ca := &x509.Certificate{
@@ -40,6 +63,7 @@ func prepCertsForTesting(t *testing.T) error {
 	if err != nil {
 		t.Log("Error creating CA private key for testing")
 		t.Error(err)
+		return certPEM, certPrivKeyPEM, caPEM, err
 	}
 
 	// create CA cert
@@ -47,10 +71,11 @@ func prepCertsForTesting(t *testing.T) error {
 	if err != nil {
 		t.Log("Error creating CA certificate for testing")
 		t.Error(err)
+		return certPEM, certPrivKeyPEM, caPEM, err
 	}
 
 	// PEM Encode CA cert
-	caPEM := new(bytes.Buffer)
+	caPEM = new(bytes.Buffer)
 	pem.Encode(caPEM, &pem.Block{
 		Type:  "CERTIFICATE",
 		Bytes: caBytes,
@@ -85,6 +110,7 @@ func prepCertsForTesting(t *testing.T) error {
 	if err != nil {
 		t.Log("Error creating private key for testing")
 		t.Error(err)
+		return certPEM, certPrivKeyPEM, caPEM, err
 	}
 
 	// self-sign the cert
@@ -92,19 +118,90 @@ func prepCertsForTesting(t *testing.T) error {
 	if err != nil {
 		t.Log("Error creating server certificate for testing")
 		t.Error(err)
+		return certPEM, certPrivKeyPEM, caPEM, err
 	}
 
 	// PEM Encode server cert
-	certPEM := new(bytes.Buffer)
+	certPEM = new(bytes.Buffer)
 	pem.Encode(certPEM, &pem.Block{
 		Type:  "CERTIFICATE",
 		Bytes: certBytes,
 	})
-	certPrivKeyPEM := new(bytes.Buffer)
+	certPrivKeyPEM = new(bytes.Buffer)
 	pem.Encode(certPrivKeyPEM, &pem.Block{
 		Type:  "RSA PRIVATE KEY",
 		Bytes: x509.MarshalPKCS1PrivateKey(certPrivKey),
 	})
 
-	return nil
+	return certPEM, certPrivKeyPEM, caPEM, err
+}
+
+func startTLSServer(t *testing.T, wg *sync.WaitGroup, addr string) {
+
+	// prep certs for test
+	t.Log("creating self-signed cert for testing")
+	certPEM, certPrivKeyPEM, caPEM, err := prepCertsForTesting(t)
+	if err != nil {
+		t.Log("Error creating self-signed cert for testing")
+		t.Error(err)
+	}
+	assert.NoError(t, err)
+
+	// prep x509 keypair for test
+	t.Log("creating self-signed x509 keypair for testing")
+	serverCert, err := tls.X509KeyPair(certPEM.Bytes(), certPrivKeyPEM.Bytes())
+	if err != nil {
+		t.Log("Error creating self-signed x509 keypair for testing")
+		t.Error(err)
+	}
+	assert.NoError(t, err)
+
+	// prep cert pool
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM(caPEM.Bytes())
+
+	// tls config for testing server
+	tlsConfig := tls.Config{}
+	tlsConfig.Certificates = []tls.Certificate{serverCert}
+
+	// start TLS server
+	t.Log("start tls.Listen")
+	tlsListener, err := tls.Listen("tcp", addr, &tlsConfig)
+	if err != nil {
+		t.Error(err)
+	}
+	defer tlsListener.Close()
+
+	wg.Done()
+
+	// handle incoming connections
+	for {
+		c, err := tlsListener.Accept()
+		if err != nil {
+			t.Error(err)
+		}
+		defer c.Close()
+		go connHandler(t, c)
+	}
+}
+
+func TestStunnelConnect(t *testing.T) {
+
+	// start test server
+	t.Log("starting test SSL server")
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go startTLSServer(t, &wg, "127.0.0.1:32345")
+	t.Log("waiting for test SSL server to start")
+	wg.Wait()
+
+	// test stunnelConnect
+	t.Log("test stunnelConnect")
+	c, err := stunnelConnect("test", "127.0.0.1:32345", "7BE5F7FD-BA97-4280-9B15-4F0746D875DA")
+	if err != nil {
+		t.Error(err)
+	}
+	assert.NoError(t, err)
+	defer c.Close()
+
 }
